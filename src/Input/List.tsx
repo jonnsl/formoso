62f16f0348d3926b3639d5c28ee4036aeb8a77db
Jonnathan Soares <jonnsl@hotmail.com>
2024-09-05 13:55:09 -0300

    Focus recently added question

--- src/Input/List.tsx
+++ src/Input/List.tsx
@@ -1,5 +1,5 @@
 
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useRef } from 'react'
 import InputComponent, { Input, duplicateInput } from './Input'
 import { replaceAt, remove } from '../Immutable'
 
@@ -10,7 +10,49 @@ type InputListProps = {
 }
 
 export default function InputList (props: InputListProps): ReactNode {
-  const { onInputFocus, onChange } = props
+  const { inputs, onInputFocus, onChange } = props
+
+  const previousRef = useRef<Input[]>()
+  const listRef = useRef<HTMLDivElement>(null)
+
+  const focusInput = (index: number) => {
+    if (listRef.current) {
+      const inputElement = listRef.current.childNodes[index]
+      if (inputElement && inputElement instanceof HTMLElement) {
+        inputElement.querySelector('textarea')?.focus()
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (previousRef.current) {
+      const prevInputs = previousRef.current
+
+      // Check if a new input was added
+      if (prevInputs.length < inputs.length) {
+        for (let i = 0, l = inputs.length; i < l; i++) {
+          if (prevInputs[i] !== inputs[i]) {
+            focusInput(i)
+            break
+          }
+        }
+      }
+
+      // Check if a input was removed
+      else if (prevInputs.length > inputs.length) {
+        for (let i = 0, l = prevInputs.length; i < l; i++) {
+          if (prevInputs[i] !== inputs[i]) {
+            focusInput(i === 0 ? 0 : i - 1)
+            break
+          }
+        }
+      }
+    }
+
+    return () => {
+      previousRef.current = inputs
+    }
+  })
 
   const renderInput = (input: Input, index: number): ReactNode => {
     const onInputChange = (value: Input): void => {
@@ -37,7 +79,7 @@ export default function InputList (props: InputListProps): ReactNode {
   }
 
   return (
-    <div>
+    <div ref={listRef}>
       { props.inputs.map(renderInput) }
     </div>
   )
